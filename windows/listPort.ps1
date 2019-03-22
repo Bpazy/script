9dd1e5fb3023588ed87f62e57b01576a70e8c3a7
ziyuan <hanziyuan08@outlook.com>
2019-03-22 10:00:18 +0800

    Revert "try catch"
    This reverts commit 9c3dba936f034da461b208af2601ab860be6d9d6.

--- windows/listPort.ps1
+++ windows/listPort.ps1
@@ -1,10 +1,6 @@
 foreach ($port in $args) {
-    try {
-        $process = Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
-        $processId = $process.Id
-        $processName = $process.Name
-        Write-Output "port: ${port}, processName: ${processName}, processId: ${processId}"
-    } catch {
-        Write-Output "No process bind ${port}"
-    }
+    $process = Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
+    $processId = $process.Id
+    $processName = $process.Name
+    Write-Output "port: ${port}, processName: ${processName}, processId: ${processId}"
 }
