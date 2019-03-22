9c3dba936f034da461b208af2601ab860be6d9d6
ziyuan <hanziyuan08@outlook.com>
2019-03-22 09:51:36 +0800

    try catch

--- windows/listPort.ps1
+++ windows/listPort.ps1
@@ -1,6 +1,10 @@
 foreach ($port in $args) {
-    $process = Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
-    $processId = $process.Id
-    $processName = $process.Name
-    Write-Output "port: ${port}, processName: ${processName}, processId: ${processId}"
+    try {
+        $process = Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
+        $processId = $process.Id
+        $processName = $process.Name
+        Write-Output "port: ${port}, processName: ${processName}, processId: ${processId}"
+    } catch {
+        Write-Output "No process bind ${port}"
+    }
 }
