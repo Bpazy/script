74e3edd3aa25d15dbf1e299f80c694123bc914e0
ziyuan <hanziyuan08@outlook.com>
2019-04-21 17:34:12 +0800

    kill alibaba protect

---
+++ windows/killAli.ps1
@@ -0,0 +1,12 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+
+wmic process where "name='AlibabaProtect.exe'" delete
+taskkill /t /f /im AliLangClient.exe
+taskkill /t /f /im AliGuardImplementModule.exe
+taskkill /t /f /im AliLang.exe
+taskkill /t /f /im AlibabaprotectUI.exe
