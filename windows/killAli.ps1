55cc1e1c6fc827d2bfdc3fc24174b88a7cd821b2
ziyuan <hanziyuan08@outlook.com>
2019-04-24 17:36:32 +0800

    upgrade killAli

--- windows/killAli.ps1
+++ windows/killAli.ps1
@@ -5,10 +5,37 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     Break
 }
 
+Write-Output 'Stop the services' 
 wmic process where "name='AlibabaProtect.exe'" delete
 wmic process where "name='EntSafeSvr.exe'" delete
-taskkill /t /f /im AliLangClient.exe
-taskkill /t /f /im AliGuardImplementModule.exe
+
+Write-Output 'Kill the processes'
 taskkill /t /f /im AliLang.exe
+taskkill /t /f /im AliLangDaemon.exe
+taskkill /t /f /im AliLangClient.exe # TODO permission denided
+taskkill /t /f /im AliGuardImplementModule.exe
 taskkill /t /f /im AlibabaprotectUI.exe
 taskkill /t /f /im EntSafeUI.exe
+
+Write-Output 'Clean the registry'
+$run = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\'
+Remove-ItemProperty $run AliLang # TODO permission denided
+Remove-ItemProperty $run AliLangClient
+
+$service = 'HKLM:\SYSTEM\CurrentControlSet\Services'
+Remove-Item -Force -recurse $service\AlibabaProtect
+Remove-Item -Force -recurse $service\alilangprot
+Remove-Item -Force -recurse $service\AliPaladin
+Remove-Item -Force -recurse $service\AliSystemSrv
+Remove-Item -Force -recurse $service\DsFs
+Remove-Item -Force -recurse $service\Dsns
+
+$outlook = 'HKEY_CURRENT_USER\Software\Microsoft\Office\Outlook\Addins\'
+Remove-Item -Force -recurse $outlook\DlpOutlookAddin.DlpOutlookSensor
+
+$aliLangOverIcon = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\      AliLangOverIcon\'
+$aliLangOverIconId = $aliLangOverIcon.'(default)'
+Remove-ItemProperty HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\ShellExecuteHooks\ $aliLangOverIconId
+Write-Output 'Done'
+
+pause
