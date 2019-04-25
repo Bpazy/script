31e19b2a4586c5b3e2f484a298817d1cad6be0e8
ziyuan <hanziyuan08@outlook.com>
2019-04-25 12:17:49 +0800

    modify git config

--- config/git_config.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git config --global user.name ziyuan
-git config --global user.email hanziyuan08@outlook.com
-git config --global push.default matching
-git config --global core.autocrlf input
-echo initialized
--- config/git_config.sh
+++ config/git_config.sh
@@ -1,5 +1,5 @@
 git config --global user.name ziyuan
 git config --global user.email hanziyuan08@outlook.com
-git config --global push.default matching
+git config --global push.default current 
 git config --global core.autocrlf input
 echo initialized
