205fe88a55d06f41c34edf29a6dd015696efcbaf
ziyuan <hanziyuan08@outlook.com>
2021-01-23 09:55:22 +0800

    try scoop

---
+++ windows/initDev.ps1
@@ -0,0 +1,3 @@
+Write-Output 请确保 initScoop.ps1 已被执行
+Write-Output 正在安装 Go
+scoop install -g go 
---
+++ windows/initScoop.ps1
@@ -0,0 +1,4 @@
+Write-Output 正在设置 Scoop 代理
+scoop config proxy 127.0.0.1:10809
+Write-Output 正在安装 sudo 
+scoop install -g sudo 
