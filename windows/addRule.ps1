bdf233fde373b01dd6ccaffe6115b7a262870b63
ziyuan <hanziyuan08@outlook.com>
2022-01-12 23:12:30 +0800

    add clash rule ps1

--- windows/addRule.ps1
+++ windows/addRule.ps1
@@ -1,12 +1,20 @@
 # 不支持 Service Mode
-taskkill /f /im "Clash for Windows.exe"
-taskkill /f /im "clash-win64.exe"
+Stop-Process -Name "Clash for Windows"
+Stop-Process -Name "clash-win64"
 
 if ($args.Count -eq 0) {
     return
 }
 
-$newRule = "`r`n  - DOMAIN-SUFFIX," + $args[0]
+if ($args[0].StartsWith("http://") -or $args[0].StartsWith("https://")) {
+    $arr = ([System.Uri]$args[0]).Host -split "\."
+    $domain = $arr[-2..-1] -join "."
+} else {
+    $arr = $args[0] -split "\."
+    $domain = $arr[-2..-1] -join "."
+}
+
+$newRule = "  - DOMAIN-SUFFIX," + $domain
 Write-Output $newRule
 Add-Content -Path "C:\Users\hanzi\.config\clash\ruleset\myproxy.yaml" -Value $newRule
-Start-Process "C:\Users\hanzi\AppData\Local\Programs\Clash for Windows\Clash for Windows.exe"
+Start-Process -RedirectStandardOutput nul "C:\Users\hanzi\AppData\Local\Programs\Clash for Windows\Clash for Windows.exe"
