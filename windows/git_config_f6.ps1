5f22455d3b6aec07b86ff9ac856ea564c9b7a813
Ziyuan Han <hanziyuan08@outlook.com>
2023-02-22 14:58:27 +0800

    增加 git 配置

---
+++ windows/git_config_f6.ps1
@@ -0,0 +1,3 @@
+git config user.name hanziyuan
+git config user.email hzy195081@ncarzone.com
+echo initialized
---
+++ windows/git_config_global.ps1
@@ -0,0 +1,4 @@
+git config --global --replace-all user.name "Ziyuan Han"
+git config --global user.email hanziyuan08@outlook.com
+git config --global push.default current 
+echo initialized
